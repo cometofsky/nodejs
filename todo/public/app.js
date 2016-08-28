e554f5a9257f24d863387f3c550d33f834739320
Faozul Azim Rafi <me@rafi.pro>
2016-08-28 22:59:31 +0600

    just uploading hazardous files and codes

---
+++ todo/public/app.js
@@ -0,0 +1,90 @@
+/**
+ * **EVENT Create - START** *
+var events = require('events');
+
+
+var myEmitter = new events.EventEmitter();
+
+
+myEmitter.on('myEvent', function(msg) {
+    console.log(msg);
+});
+
+
+myEmitter.emit('myEvent', 'hello rafi');
+
+* **EVENT Create - END** *
+**/
+
+
+/**
+ * **EVENT HOOK - START** *
+var events = require('events');
+var util = require('util');
+
+
+var Person = function(name) {
+    this.name = name;
+};
+
+
+util.inherits(Person, events.EventEmitter);
+
+
+var rafi = new Person('rafi');
+var rafi2 = new Person('rafi2');
+
+
+var people = [
+    rafi, rafi2
+];
+
+
+people.forEach(function(value) {
+    value.on('speak', function(speech) {
+        console.log(value.name + ' said: ' + speech)
+    });
+});
+
+
+rafi.emit('speak', 'bismillah');
+rafi2.emit('speak', 'bismillah');
+
+
+* **EVENT HOOK - END** *
+**/
+
+
+/**
+ * **READING & WRITING A FILE - START** *
+var fs = require('fs');
+
+
+//fs.readFileSync() && fs.writeFileSync for asynchronous behaviour
+fs.readFile('readme.md', 'utf8', function(err, data) {
+    fs.writeFile('readme.md2', data);
+});
+
+
+* **READING & WRITING A FILE - END** *
+**/
+
+
+/**
+ * CREATING & DELETING***- START* *
+ */
+var fs = require('fs');
+
+//fs.mkdirSync('deleting');
+//fs.rmdirSync() for synchronous behaviour
+/*fs.mkdir('deleting', function() {
+    fs.readFile('readme.md', 'utf8', function(err, data) {
+        fs.writeFile('./deleting/readme.md', data);
+    });
+});*/
+fs.unlink('./deleting/readme.md', function() {
+    fs.rmdir('deleting');
+});
+ /*
+ * CREATING & DELETING***- END* *
+ **/
---
+++ todo/public/readme.md
@@ -0,0 +1,1 @@
+Introductory tutorial for nodeJS beginner
